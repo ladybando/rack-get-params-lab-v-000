3e987df22f5f5f593cca77d0bd81de0aeb445f10
Ayana Bando <ayanaflax@yahoo.com>
2018-09-22 15:06:28 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -18,6 +18,7 @@ class Application
     elsif req.path.match(/cart/)
        if @@cart.empty?
          resp.write "Your cart is empty"
+       end
     else 
         @@cart.each do |item|
         resp.write "#{item}\n"
