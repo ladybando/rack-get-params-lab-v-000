3716fd1c59136b68bb1381cec0a4a1296e0971ca
Ayana Bando <ayanaflax@yahoo.com>
2018-09-22 15:01:10 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -16,7 +16,7 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-       if items.each do |item|
+       if @@cart.include?(items)
          return "#{item}\n"
        end
     else
