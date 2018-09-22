d7a0deefc33f9ce026d9692f0db1ffb5f29b510d
Ayana Bando <ayanaflax@yahoo.com>
2018-09-22 15:01:21 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -17,7 +17,7 @@ class Application
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
        if @@cart.include?(@@items)
-         return "#{item}\n"
+         return "#{@@items}\n"
        end
     else
       return "Your cart is empty"
